f2881a2a12dd3d004db2978e35ec6122751a7821
Farhan Anshari Miraj <farhananshari77@gmail.com>
2021-06-03 14:19:38 +0700

    update layout & add controller

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('user.index');
 });
+
+Route::get('/dashboard', [App\Http\Controllers\MasterController::class, 'index'])->name('dashboard');
